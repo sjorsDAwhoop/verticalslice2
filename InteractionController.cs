c7d401a7cec1dcaa3e0f0dafa08f4f4121b40e44
Sjors van der Kleij <24689@ma-web.nl>
2019-02-01 15:53:11 +0100

    first commit

---
+++ InteractionController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InteractionController : MonoBehaviour
+{
+
+    private GameObject hold;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+    void FixedUpdate()
+    {
+     
+       
+        if (hold != null) { hold.transform.position = this.gameObject.transform.GetChild(0).transform.position;
+            if (Input.GetMouseButtonDown(1)) { if (hold.GetComponent<Rigidbody>()) { hold.GetComponent<Rigidbody>().useGravity = true; } hold = null; }}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            if (Physics.Raycast(transform.position, fwd, out hit, 5f))
+            {  
+
+                switch (hit.collider.gameObject.tag) { 
+                        case "PickUp": hold = hit.collider.gameObject;if (hold.GetComponent<Rigidbody>()) { hold.GetComponent<Rigidbody>().useGravity = false; } break;
+                        case "UpDown": case "LeftRight": case "RightLeft": hit.transform.parent.GetComponent<PlatformMovementController>().auto = false; hit.transform.parent.GetComponent<PlatformMovementController>().tag = hit.collider.tag; break; 
+                }
+            }
+                    
+        }
+    }
+}
---
+++ PlatformMovementController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementController : MonoBehaviour {
+
+    public string tag;
+    public bool auto;
+    private GameObject platform;
+    private float posY, posX;
+    [SerializeField] private string Go;
+    [SerializeField] private float distance;
+    private void Start()
+    {
+        platform = transform.parent.gameObject;
+        auto = false;
+        posY = Mathf.Round(platform.transform.position.y);
+        posX = Mathf.Round(platform.transform.position.x);
+        if (distance == 0) { distance = 3; }
+
+
+    }
+  
+    private void OnTriggerExit(Collider col)
+    {
+        print("Exit");
+        auto = true;
+        col.transform.parent = null;
+
+    }
+  
+    private void OnTriggerStay(Collider col)
+    {
+    
+        auto = false;
+        col.transform.parent = platform.transform;
+        Run();
+    }
+    private void Update()
+    {
+        moveTo();
+        if (auto) {  Run();  }
+
+    }
+  
+    void Run()
+    {
+  
+            switch (Go) {
+            case "UP":platform.transform.Translate(Vector3.up * Time.deltaTime*2); break;
+            case "DOWN":platform.transform.Translate(Vector3.down * Time.deltaTime*2); break;
+            case "LEFT":platform.transform.Translate(Vector3.left * Time.deltaTime*3); break;
+            case "RIGHT":platform.transform.Translate(Vector3.right * Time.deltaTime*3); break; }
+
+    }
+
+    void moveTo()
+    {
+        switch (tag)
+        {
+            case "UpDown":
+                if (Mathf.Round(platform.transform.position.y) == posY) { Go = "UP"; }
+                else if (Mathf.Round(platform.transform.position.y) == (posY + distance)) { Go = "DOWN"; }
+                break;
+            case "LeftRight":
+                if (Mathf.Round(platform.transform.position.x) == posX) { Go = "LEFT"; }
+                else if (Mathf.Round(platform.transform.position.x) == (posX + distance)) { Go = "RIGHT"; }
+                break;
+            case "RightLeft":
+                if (Mathf.Round(platform.transform.position.x) == posX) { Go = "RIGHT"; }
+                else if (Mathf.Round(platform.transform.position.x) == (posX + distance)) { Go = "LEFT"; }
+                break;
+        }
+
+    }
+}
+
+
+
